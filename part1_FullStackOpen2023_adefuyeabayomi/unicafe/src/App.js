ef8e84b6011cc6a2d645fe97baf6b6335aaf3e52
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2023-08-14 17:19:25 +0100

    Completed exercise 1.8

--- part1_FullStackOpen2023_adefuyeabayomi/unicafe/src/App.js
+++ part1_FullStackOpen2023_adefuyeabayomi/unicafe/src/App.js
@@ -5,22 +5,40 @@ const Button = (props) => {
     <button onClick={props.handleClick}>{props.text}</button>
   )
 }
+
+const Statistics = (props) => {
+  function average () { 
+    // average is given by summation fx / summation f 
+    // f are the values good, neutral, bad . Summation F is 3 
+    // fx are the corresponding values of F 
+    // Using this explanation, we implement the average as given below 
+    let {good,bad,neutral} = props;
+    return (good + bad + neutral) / (3) ;
+  }
+  function positiveReviews () {
+    let {good,bad,neutral} = props;
+    let total = good + bad + neutral;
+    return ( good / total ) * 100; 
+  }  
+  return (
+    <div>
+      <h3>Statistics</h3>
+      <p>Good : {props.good}</p>
+      <p>Bad : {props.bad} </p>
+      <p>Neutral : {props.neutral} </p>
+      <p>Total Reviews : {props.good + props.bad + props.neutral} </p>
+      <p>Average : {average()}</p>
+      <p>Positive reviews : {positiveReviews()}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  function average () {
-    // average is given by summation fx / summation f
-    // f are the values good, neutral, bad . Summation F is 3
-    // fx are the corresponding values of F 
-    // Using this explanation, we implement the average as given below
-    return (good + bad + neutral) / (3) ;
-  }
-  function positiveReviews () {
-    let total = good + bad + neutral;
-    return ( good / total ) * 100; 
-  }
+
   return (
     <div>
       <h1>Your feedback helps us improve.</h1>
@@ -28,13 +46,7 @@ const App = () => {
       <Button text="good" handleClick={() => setGood(good + 1)} />
       <Button text="bad" handleClick={() => setBad(bad + 1)} />
       <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
-      <h3>Statistics</h3>
-      <p>Good : {good}</p>
-      <p>Bad : {bad} </p>
-      <p>Neutral : {neutral} </p>
-      <p>Total Reviews : {good + bad + neutral} </p>
-      <p>Average : {average()}</p>
-      <p>Positive reviews : {positiveReviews()}%</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
