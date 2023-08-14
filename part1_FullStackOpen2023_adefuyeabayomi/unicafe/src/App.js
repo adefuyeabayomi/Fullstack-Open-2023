62568b94646b71085d7c958b7eb8db302f21021e
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2023-08-14 16:51:40 +0100

    Commit after exercise 1.7

--- part1_FullStackOpen2023_adefuyeabayomi/unicafe/src/App.js
+++ part1_FullStackOpen2023_adefuyeabayomi/unicafe/src/App.js
@@ -10,7 +10,17 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  function average () {
+    // average is given by summation fx / summation f
+    // f are the values good, neutral, bad . Summation F is 3
+    // fx are the corresponding values of F 
+    // Using this explanation, we implement the average as given below
+    return (good + bad + neutral) / (3) ;
+  }
+  function positiveReviews () {
+    let total = good + bad + neutral;
+    return ( good / total ) * 100; 
+  }
   return (
     <div>
       <h1>Your feedback helps us improve.</h1>
@@ -22,6 +32,9 @@ const App = () => {
       <p>Good : {good}</p>
       <p>Bad : {bad} </p>
       <p>Neutral : {neutral} </p>
+      <p>Total Reviews : {good + bad + neutral} </p>
+      <p>Average : {average()}</p>
+      <p>Positive reviews : {positiveReviews()}%</p>
     </div>
   )
 }
