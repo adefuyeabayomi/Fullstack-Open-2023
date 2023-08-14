99a326274c103e0239a01c6f5d7c61972e3c4994
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2023-08-14 16:36:08 +0100

    completed exercise 1.6

--- part1_FullStackOpen2023_adefuyeabayomi/unicafe/src/App.js
+++ part1_FullStackOpen2023_adefuyeabayomi/unicafe/src/App.js
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +13,15 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>Your feedback helps us improve.</h1>
+      <p>How do you feel about the services rendered to you?</p>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <h3>Statistics</h3>
+      <p>Good : {good}</p>
+      <p>Bad : {bad} </p>
+      <p>Neutral : {neutral} </p>
     </div>
   )
 }
