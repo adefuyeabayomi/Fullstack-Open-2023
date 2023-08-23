dade24c382ba6c4af6ed4f40c32b0ab49f6b59d2
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2023-08-23 06:39:18 +0100

    Completed exercise 3.9 Updated the url to the backend

--- part2_FullStackOpen2023_adefuyeabayomi/phonebook/src/services/utility.js
+++ part2_FullStackOpen2023_adefuyeabayomi/phonebook/src/services/utility.js
@@ -1,18 +1,18 @@
 import axios from "axios";
   const submitToServer = (newEntry) => {
-    let request = axios.post("http://localhost:3001/persons",newEntry);
+    let request = axios.post("http://localhost:3001/api/persons",newEntry);
     return request;
   }
   const getAllPersons = () => {
-    let request = axios.get("http://localhost:3001/persons");
+    let request = axios.get("http://localhost:3001/api/persons");
     return request;
   }
   const deleteEntry = (id) => {
-    let request = axios.delete("http://localhost:3001/persons/"+id);
+    let request = axios.delete("http://localhost:3001/api/persons/"+id);
     return request;
   }
   const updateEntry = (id,updatedEntry) => {
-    let request = axios.put("http://localhost:3001/persons/"+id,updatedEntry)
+    let request = axios.put("http://localhost:3001/api/persons/"+id,updatedEntry)
     return request;
   }
   export default {submitToServer,getAllPersons,deleteEntry,updateEntry};
